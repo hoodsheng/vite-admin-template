7e6126be4b3279968ca343138ecf3aaef5f90f28
hoodsheng <hoodsheng@163.com>
2022-09-13 11:36:04 +0800

    feat: 🚀 element-plus按需引入

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,69 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ElMessage } from "element-plus";
+import { ref } from "vue";
+
+const value = ref("");
+
+const options = [
+	{
+		value: "Option1",
+		label: "Option1"
+	},
+	{
+		value: "Option2",
+		label: "Option2"
+	},
+	{
+		value: "Option3",
+		label: "Option3"
+	},
+	{
+		value: "Option4",
+		label: "Option4"
+	},
+	{
+		value: "Option5",
+		label: "Option5"
+	}
+];
+
+const open1 = () => {
+	ElMessage({
+		message: "Congrats, this is a success message.",
+		type: "success"
+	});
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+	<div>
+		<el-select v-model="value" class="m-2" placeholder="Select" size="large">
+			<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+		</el-select>
+		<el-button type="primary" @click="open1">Primary</el-button>
+		<el-button type="success">Success</el-button>
+		<el-button type="info">Info</el-button>
+		<el-button type="warning">Warning</el-button>
+		<el-button type="danger">Danger</el-button>
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+	filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import "element-plus/es/components/message/style/css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
