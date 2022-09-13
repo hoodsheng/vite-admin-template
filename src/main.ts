0239c2e0b65da3ade49c9b6f9d3b780188939b6c
hoodsheng <hoodsheng@163.com>
2022-09-13 15:22:35 +0800

    feat: 🚀 基础样式，文件引入

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { setupAssets } from "./plugins";
+// element icons
+import * as Icons from "@element-plus/icons-vue";
+import "./styles/reset.scss";
+import "./styles/common.scss";
+import "./assets/iconfont/iconfont.scss";
+import "./assets/fonts/font.scss";
+import "element-plus/dist/index.css";
+// element dark(内置暗黑模式)
+import "element-plus/theme-chalk/dark/css-vars.css";
+// custom element dark(自定义暗黑模式)
+import "./styles/theme/element-dark.scss";
+import "./styles/element.scss";
+// custom directives
+import directives from "./directives/index";
 
-async function setupApp() {
-	// import assets: js、css
-	setupAssets();
+const app = createApp(App);
+// 注册element Icons组件
+Object.keys(Icons).forEach(key => {
+	app.component(key, Icons[key as keyof typeof Icons]);
+});
 
-	const app = createApp(App);
-	// mount app
-	app.mount("#app");
-}
-
-setupApp();
+app.use(directives);
+app.mount("#app");
